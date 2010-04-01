69ba9094e52107bbec5266c16fcfe909e2040acd
Jonathan Elsas <jelsas@cs.cmu.edu>
2010-03-31 22:27:59 -0400

    Adding comments

--- src/itertools/SimpleIterable.java
+++ src/itertools/SimpleIterable.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 
 /**
  * A simple Iterable wrapper anound an iterator, to help in enhanced for loops.
+ * Note: this is a single-use Iterable: subsequent calls to {@link #iterator()}
+ * will return the same, possibly empty, iterator object.
  * 
  * @author jelsas
  * 
