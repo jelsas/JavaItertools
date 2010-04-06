7823dabe8c5dea327c2482de214912ed4ab7bd3e
Jonathan Elsas <jelsas@cs.cmu.edu>
2010-04-06 13:42:02 -0400

    removing unused imports

--- src/itertools/iterator/LazyGroupingIterator.java
+++ src/itertools/iterator/LazyGroupingIterator.java
@@ -15,10 +15,7 @@
  */
 package itertools.iterator;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
